a2d88c5fe22d87a1fdf448c2885e97356626b5d4
Fabio Napoleoni <f.napoleoni@gmail.com>
2011-07-01 03:03:58 +0200

    Parametrized the session controller with resource name

--- app/controllers/google_authentication/sessions_controller.rb
+++ app/controllers/google_authentication/sessions_controller.rb
@@ -4,7 +4,7 @@ class GoogleAuthentication::SessionsController < Devise::SessionsController
 
   # send user to google apps page for authentication
   def new
-    redirect_to user_omniauth_authorize_path(:google_apps)
+    redirect_to (send "#{resource_name}_omniauth_authorize_path", :google_apps)
   end
 
 end
