2909cc07e1b2798c4a958d889d4acf19bac1849d
Fabio Napoleoni <f.napoleoni@gmail.com>
2011-07-02 01:46:49 +0200

    Fixed an issue when code is loaded and the model doesn't exist (yet)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  # Devise + omniauth routes
-  devise_for GoogleAuthentication.model_name.to_s.pluralize.to_sym, :controllers => {:omniauth_callbacks => "google_authentication/omniauth_callbacks"} do
-    get 'sign_in', :to => 'google_authentication/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'google_authentication/sessions#destroy', :as => :destroy_user_session
+  if defined?(GoogleAuthentication.model_name.constantize) || Rails.env.test?
+    # Devise + omniauth routes only if the given class exist
+    devise_for GoogleAuthentication.model_name.to_s.pluralize.to_sym, :controllers => {:omniauth_callbacks => "google_authentication/omniauth_callbacks"} do
+      get 'sign_in', :to => 'google_authentication/sessions#new', :as => :new_user_session
+      get 'sign_out', :to => 'google_authentication/sessions#destroy', :as => :destroy_user_session
+    end
   end
 
 end
