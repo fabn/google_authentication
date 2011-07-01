cfabce36f5290d424b3bf83abbacd2c37b8f4d00
Fabio Napoleoni <f.napoleoni@gmail.com>
2011-07-01 02:31:11 +0200

    Working initialization code for devise omniauth configuration

--- lib/google_authentication.rb
+++ lib/google_authentication.rb
@@ -1,8 +1,21 @@
 module GoogleAuthentication
 
   # domain configuration
-  #mattr_accessor :domain
-  #@@domain = "gmail.com"
+  mattr_accessor :domain
+  # default value for google domain used for authentication
+  @@domain = "gmail.com"
 
+  # Allows config in initializer
+  # @yield [self] Allows config in initializer using the same syntax as Devise
+  # @yieldparam [GoogleAuthentication] config the module itself
+  # @example Changing the authentication domain
+  #   GoogleAuthentication.setup do |config|
+  #     config.domain = 'your-google-apps-domain.com'
+  #   end
+  def self.setup
+    yield self
+  end
+
+  # require the engine if rails is defined
   require 'google_authentication/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
